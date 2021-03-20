a862092b6ef1782c9799be036eccc4122ac1ad2e
ychi111 <34000828+ychi111@users.noreply.github.com>
2021-03-21 02:16:18 +0800

    Update animls.php

--- animls.php
+++ animls.php
@@ -1,17 +1,51 @@
 <?php
 
-abstract class animal{
-    // Статическая переменая для хранения ключа
-    static $id = 1;
-    // номер животного
-    public $idAnimal=0;
-    // сколько дает
+abstract class animal {
+    public $animalId;
     public abstract function getProduct();
-    // вернуть
     public function getAnimalType()
     {
         return static::class;
     }
+    public function __construct(id)
+    {
+        $this->animalId = id;
+    }
 }
+
+/**
+ * Класс курица
+ */
+class chicken extends animal{
+    // коснтруктор класса, задаем уникальный номер
+    function __construct() {
+        // Задаем уникальный номер
+        $this->idAnimal=parent::$id++;
+        //Задаем уникальный номер
+        // print "chicken id-".$this->idAnimal." \n";
+    }
+    // сколько может дать яйца курица
+    public function getProduct(){
+        return rand(0,1);
+    }
+
+}
+
+/**
+ * Класс корова
+ */
+class cow extends animal{
+    // коснтруктор класса, задаем уникальный номер
+    function __construct() {
+        // задаем номер уникальный
+        $this->idAnimal=parent::$id++;
+        // print "Конструктор класса cow". $this->idAnimal." \n";
+    }
+
+    // сколько может дать корова молоко
+    public function getProduct(){
+        return rand(8, 12);
+    }
+}  
 
 ?>
